f6012f9d5e9af96dce7be84b6a322c33aa01cef6
Maximilian Kaske <maximilian@kaske.org>
2021-05-26 15:59:59 +0200

    chore: load font on app start

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { NavigationContainer } from "./src/navigation";
+import StackNavigation from "./src/navigation/StackNavigation";
 import { ThemeProvider } from "./src/themes";
+
+const fonts = {
+  "OpenSans-Bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  "OpenSans-BoldItalic": require("./assets/fonts/OpenSans-BoldItalic.ttf"),
+  "OpenSans-Light": require("./assets/fonts/OpenSans-Light.ttf"),
+  "OpenSans-LightItalic": require("./assets/fonts/OpenSans-LightItalic.ttf"),
+  "OpenSans-Regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+  "OpenSans-Italic": require("./assets/fonts/OpenSans-Italic.ttf"),
+};
 
 export default function App() {
   return (
     <ThemeProvider>
-      <NavigationContainer />
+      <NavigationContainer {...{ fonts }}>
+        <StackNavigation />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
--- src/navigation/NavigationContainer.tsx
+++ src/navigation/NavigationContainer.tsx
@@ -1,14 +1,102 @@
-import React, { FC } from "react";
-import { NavigationContainer as RNNavigationContainer } from "@react-navigation/native";
-import StackNavigation from "./StackNavigation";
-import { Box } from "../themes";
+import React, {
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+  useRef,
+} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import {
+  InitialState,
+  NavigationContainer,
+  NavigationContainerRef,
+} from "@react-navigation/native";
+import Constants from "expo-constants";
 
-const NavigationContainer: FC = () => {
+const NAVIGATION_STATE_KEY = `NAVIGATION_STATE_KEY-${Constants.manifest.sdkVersion}`;
+
+export type FontSource = Parameters<typeof Font.loadAsync>[0];
+
+const usePromiseAll = (
+  promises: Promise<void | void[] | Asset[]>[],
+  cb: () => void
+) =>
+  useEffect(() => {
+    (async () => {
+      await Promise.all(promises);
+      cb();
+    })();
+  });
+
+const useLoadAssets = (assets: number[], fonts: FontSource): boolean => {
+  const [ready, setReady] = useState(false);
+  usePromiseAll(
+    [Font.loadAsync(fonts), ...assets.map((asset) => Asset.loadAsync(asset))],
+    () => setReady(true)
+  );
+  return ready;
+};
+
+interface LoadAssetsProps {
+  fonts?: FontSource;
+  assets?: number[];
+  children: ReactElement | ReactElement[];
+}
+
+const LoadAssets = ({ assets, fonts, children }: LoadAssetsProps) => {
+  const [isNavigationReady, setIsNavigationReady] = useState(!__DEV__);
+  const [initialState, setInitialState] = useState<InitialState | undefined>();
+
+  const routeNameRef = useRef<string | undefined>();
+  const navigationRef = useRef<NavigationContainerRef>(null);
+
+  const ready = useLoadAssets(assets || [], fonts || {});
+
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const savedStateString = await AsyncStorage.getItem(
+          NAVIGATION_STATE_KEY
+        );
+        const state = savedStateString
+          ? JSON.parse(savedStateString)
+          : undefined;
+        setInitialState(state);
+      } finally {
+        setIsNavigationReady(true);
+      }
+    };
+    if (!isNavigationReady) {
+      restoreState();
+    }
+  }, [isNavigationReady]);
+
+  const onStateChange = useCallback((state) => {
+    AsyncStorage.setItem(NAVIGATION_STATE_KEY, JSON.stringify(state));
+  }, []);
+
+  const onReady = () => {
+    routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name;
+  };
+
+  if (!ready || !isNavigationReady) {
+    return null;
+  }
+
   return (
-    <RNNavigationContainer>
-      <StackNavigation />
-    </RNNavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      {...{
+        onReady,
+        onStateChange,
+        initialState,
+      }}
+    >
+      {children}
+    </NavigationContainer>
   );
 };
 
-export default NavigationContainer;
+export default LoadAssets;
