0782560d4b9bf722f7e06185daac987066eda9c8
Maximilian Kaske <maximilian@kaske.org>
2021-05-28 17:09:47 +0200

    refactor: move SchemeSettings back to Home screen

--- src/components/home/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as SchemeSettings } from "./SchemeSettings";
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,22 +1,50 @@
 import React, { FC } from "react";
+import { useColorScheme } from "react-native";
 import { useQueryClient } from "react-query";
-import { SchemeSettings } from "../components/home";
-import { Button } from "../components/ui";
+import { useDispatch } from "react-redux";
+import { Button, Switch } from "../components/ui";
 import { AppNavigationProps } from "../navigation";
+import { useSelector } from "../store";
+import { resetColorScheme, setColorScheme } from "../store/global/actions";
 import { Box, Text } from "../themes";
 
 const Home: FC<AppNavigationProps<"Home">> = ({ navigation, route }) => {
   const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const colorScheme = useSelector((state) => state.global.colorScheme);
+  const deviceColorScheme = useColorScheme();
+  const darkMode = colorScheme
+    ? colorScheme === "dark"
+    : deviceColorScheme === "dark";
+
   return (
     <Box flex={1} padding="l" alignItems="center" justifyContent="center">
-      <Button label="reset" onPress={() => queryClient.resetQueries()} />
-      <Text variant="title">Home</Text>
-      <Button
-        label="go to TodoList"
-        marginTop="m"
-        onPress={() => navigation.push("TodoList")}
-      />
-      <SchemeSettings />
+      <Box flex={1} alignItems="center" justifyContent="center">
+        <Button label="reset" onPress={() => queryClient.resetQueries()} />
+        <Button
+          label="go to TodoList"
+          marginTop="m"
+          onPress={() => navigation.push("TodoList")}
+        />
+      </Box>
+      <Box flex={1} alignItems="center" justifyContent="center">
+        <Text>Current Mode {colorScheme || "default colorScheme"}</Text>
+        <Box flexDirection="row" alignItems="center">
+          <Text padding="l">Dark Mode: {darkMode ? "on" : "off"}</Text>
+          <Switch
+            value={darkMode}
+            onValueChange={(value: boolean) => {
+              dispatch(setColorScheme(value ? "dark" : "light"));
+            }}
+          />
+        </Box>
+        <Box flexDirection="row" alignItems="center">
+          <Button
+            label={`use device color scheme (${deviceColorScheme})`}
+            onPress={() => dispatch(resetColorScheme())}
+          />
+        </Box>
+      </Box>
     </Box>
   );
 };
